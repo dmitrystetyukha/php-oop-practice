015c340266171796341b9d58391c36c40277aab1
Dmitry Stetyukha <s.dmitriy@webpractik.ru>
2023-08-03 16:02:40 +0300

    add templates add index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/src/header.html';
+
+require_once 'vendor/autoload.php';
+
+require_once __DIR__ . '/src/template.php';
+
+require_once __DIR__ . '/src/footer.html';
